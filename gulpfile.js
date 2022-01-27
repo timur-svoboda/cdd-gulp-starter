57d2a945d118ba4b3edb4092bcee9c8e5170498d
Timur Svoboda <svobodatimur@gmail.com>
2022-01-27 20:09:26 +0200

    Add publish task to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
 import devTask from "./gulp/dev/dev.js";
 import prodTask from "./gulp/prod/prod.js";
+import publishTask from "./gulp/publish/publish.js"
 
 export default devTask;
 
 export const dev = devTask;
 
 export const prod = prodTask;
+
+export const publish = publishTask;
